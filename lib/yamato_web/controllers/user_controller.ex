76d825a830dd95ac1305ba84017ca0e141af9356
Inhji <johnnie@posteo.de>
2018-05-17 22:45:37 +0200

    disallow registration when one user is already registered

--- lib/yamato_web/controllers/user_controller.ex
+++ lib/yamato_web/controllers/user_controller.ex
@@ -4,6 +4,19 @@ defmodule YamatoWeb.UserController do
   alias Yamato.Accounts
   alias Yamato.Accounts.User
 
+  plug :user_already_exists
+
+  defp user_already_exists(conn, _params) do
+    if length(Yamato.Accounts.list_users) > 0 do
+      conn
+      |> put_flash(:error, "Sorry, you can't register!")
+      |> redirect(to: "/")
+      |> halt()
+    else
+      conn
+    end
+  end
+
   def new(conn, _params) do
     changeset = Accounts.change_user(%User{})
     render(conn, "new.html", changeset: changeset)
