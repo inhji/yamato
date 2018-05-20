f50fdccad7bcbd4eb0e5909747541a59cf0e92dd
Inhji <johnnie@posteo.de>
2018-05-20 14:11:40 +0200

    complete tags!

--- lib/yamato_web/views/article_view.ex
+++ lib/yamato_web/views/article_view.ex
@@ -1,6 +1,8 @@
 defmodule YamatoWeb.ArticleView do
   use YamatoWeb, :view
 
+  def markdown(body) when is_nil(body), do: "No excerpt"
+
   def markdown(body) do
     body
     |> Earmark.as_html!
@@ -11,7 +13,18 @@ defmodule YamatoWeb.ArticleView do
     Timex.format!(date, "{YYYY}-{0M}-{D}")
   end
 
-  def tags_string(tags) do
-    Enum.join(tags, ",")
+  def tags_string(tags) when is_nil(tags), do: ""
+  def tags_string(tags), do: Enum.join(tags, ",")
+
+  def primary_tag(tags) when is_nil(tags), do: nil
+  def primary_tag([]), do: nil
+  def primary_tag([tag|_rest]) do
+    tag
+  end
+
+  def secondary_tags(tags) when is_nil(tags), do: []
+  def secondary_tags([]), do: []
+  def secondary_tags([_tag|rest]) do
+    rest
   end
 end
