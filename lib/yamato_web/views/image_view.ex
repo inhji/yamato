213fec4a4f2c381f434dd5871ccebe9da77b679c
Inhji <johnnie@posteo.de>
2018-05-26 19:57:47 +0200

    include basepath in image permalink

--- lib/yamato_web/views/image_view.ex
+++ lib/yamato_web/views/image_view.ex
@@ -1,3 +1,10 @@
 defmodule YamatoWeb.ImageView do
   use YamatoWeb, :view
+
+  def image_permalink(image, size \\ :original) do
+    Path.join([
+      YamatoWeb.Endpoint.url(),
+      Yamato.ArticleImageUploader.url({image.image, image}, size)
+    ])
+  end
 end
