f7d838e13bbd962276e0f87ddac034a28d09b55a
Inhji <johnnie@posteo.de>
2018-05-27 11:47:21 +0200

    add active_class helper

--- lib/yamato_web/views/layout_view.ex
+++ lib/yamato_web/views/layout_view.ex
@@ -9,6 +9,14 @@ defmodule YamatoWeb.LayoutView do
     end
   end
 
+  def active_class(conn, path, additonal_classes \\ []) do
+    if conn.request_path === path do
+      Enum.join(["is-active" | additonal_classes], " ")
+    else
+      Enum.join(additonal_classes, " ")
+    end
+  end
+
   def is_index?(conn) do
     conn.request_path == "/"
   end
